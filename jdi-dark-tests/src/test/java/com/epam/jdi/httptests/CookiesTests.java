4f4ade21c4afe7e75ab1b14774ee373e57d08091
TPetukhova <Tatiana_Petukhova@epam.com>
2020-01-29 13:27:10 +0300

    Added isOk() methods.

--- jdi-dark-tests/src/test/java/com/epam/jdi/httptests/CookiesTests.java
+++ jdi-dark-tests/src/test/java/com/epam/jdi/httptests/CookiesTests.java
@@ -37,28 +37,27 @@ public class CookiesTests {
 
     @Test
     public void supportsCookieStringMatchingViaDsl() {
-        JettyService.setCookies.call().assertThat().cookie("key1", "value1");
+        JettyService.setCookies.call().isOk().assertThat().cookie("key1", "value1");
     }
-
 
     @Test
     public void supportsMultipleCookieStatements() {
-        JettyService.setCookies.call().assertThat().cookie("key1", "value1").and().cookie("key2", "value2");
+        JettyService.setCookies.call().isOk().assertThat().cookie("key1", "value1").and().cookie("key2", "value2");
     }
 
     @Test
     public void supportsMultipleCookiesShortVersionUsingPlainStrings() {
-        JettyService.setCookies.call().assertThat().cookies("key1", "value1", "key3", "value3");
+        JettyService.setCookies.call().isOk().assertThat().cookies("key1", "value1", "key3", "value3");
     }
 
     @Test
     public void supportsMultipleCookiesShortVersionUsingHamcrestMatching() {
-        JettyService.setCookies.call().assertThat().cookies("key2", containsString("2"), "key3", equalTo("value3"));
+        JettyService.setCookies.call().isOk().assertThat().cookies("key2", containsString("2"), "key3", equalTo("value3"));
     }
 
     @Test
     public void supportsMultipleCookiesShortVersionUsingMixOfHamcrestMatchingAndStringMatching() {
-        JettyService.setCookies.call().assertThat().cookies("key1", containsString("1"), "key2", "value2");
+        JettyService.setCookies.call().isOk().assertThat().cookies("key1", containsString("1"), "key2", "value2");
     }
 
     @Test
@@ -66,7 +65,7 @@ public class CookiesTests {
         Map<String, String> expectedCookies = new HashMap<>();
         expectedCookies.put("key1", "value1");
         expectedCookies.put("key2", "value2");
-        JettyService.setCookies.call().assertThat().cookies(expectedCookies);
+        JettyService.setCookies.call().isOk().assertThat().cookies(expectedCookies);
     }
 
     @Test
@@ -74,7 +73,7 @@ public class CookiesTests {
         Map<String, Matcher<String>> expectedCookies = new HashMap<>();
         expectedCookies.put("key1", containsString("1"));
         expectedCookies.put("key3", equalTo("value3"));
-        JettyService.setCookies.call().assertThat().cookies(expectedCookies);
+        JettyService.setCookies.call().isOk().assertThat().cookies(expectedCookies);
     }
 
     @Test
@@ -82,17 +81,17 @@ public class CookiesTests {
         Map<String, Object> expectedCookies = new HashMap<>();
         expectedCookies.put("key1", containsString("1"));
         expectedCookies.put("key2", "value2");
-        JettyService.setCookies.call().assertThat().cookies(expectedCookies);
+        JettyService.setCookies.call().isOk().assertThat().cookies(expectedCookies);
     }
 
     @Test(expectedExceptions = AssertionError.class, expectedExceptionsMessageRegExp = ".*1 expectation failed.*")
     public void assertionErrorIsThrownWhenCookieDoesNotMatch() {
-        JettyService.setCookies.call().assertThat().cookie("key1", "value2");
+        JettyService.setCookies.call().isOk().assertThat().cookie("key1", "value2");
     }
 
     @Test(expectedExceptions = AssertionError.class, expectedExceptionsMessageRegExp = ".*1 expectation failed.*")
     public void assertionErrorIsThrownWhenCookieIsNotFound() {
-        JettyService.setCookies.call().assertThat().cookie("Not-Defined", "something");
+        JettyService.setCookies.call().isOk().assertThat().cookie("Not-Defined", "something");
     }
 
     @Test
@@ -100,7 +99,7 @@ public class CookiesTests {
         JettyService.postReflect.call(requestData(requestData ->
                 requestData.cookies = new MapArray<>(new Object[][]{
                         {"jsessionid", "9HTaCatOIaiO7ccHojDzuxwVoIU="}
-                }))).assertThat().cookie("JSESSIONID", "9HTaCatOIaiO7ccHojDzuxwVoIU=");
+                }))).isOk().assertThat().cookie("JSESSIONID", "9HTaCatOIaiO7ccHojDzuxwVoIU=");
     }
 
     @Test

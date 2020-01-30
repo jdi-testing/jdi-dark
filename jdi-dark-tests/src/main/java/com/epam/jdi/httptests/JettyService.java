ef05816484f74bb4fcc7994bd826653e8c3b5848
Yulia_Litvin <iulia.litwin@yandex.ru>
2020-01-30 17:54:41 +0300

    add post tests

--- jdi-dark-tests/src/main/java/com/epam/jdi/httptests/JettyService.java
+++ jdi-dark-tests/src/main/java/com/epam/jdi/httptests/JettyService.java
@@ -1,9 +1,13 @@
 package com.epam.jdi.httptests;
 
+import com.epam.http.annotations.ContentType;
 import com.epam.http.annotations.GET;
+import com.epam.http.annotations.Header;
 import com.epam.http.annotations.POST;
 import com.epam.http.annotations.ServiceDomain;
 import com.epam.http.requests.RestMethod;
+
+import static io.restassured.http.ContentType.JSON;
 
 @ServiceDomain("http://localhost:8080/")
 public class JettyService {
@@ -22,4 +26,24 @@ public class JettyService {
 
     @GET("/setCommonIdCookies")
     static RestMethod getCommonIdCookies;
+
+    @POST("/greet")
+    static RestMethod greetPost;
+
+    @POST("/notexist")
+    static RestMethod notFoundedURIPost;
+
+    @POST("/header")
+    @Header(name = "MyHeader", value = "Something")
+    static RestMethod headerPost;
+
+    @ContentType(JSON)
+    @POST("/jsonBody")
+    static RestMethod jsonBodyPost;
+
+    @POST("/secured/hello")
+    static RestMethod unauthorizedPost;
+
+    @POST("/cookie")
+    static RestMethod cookiePost;
 }
--- jdi-dark-tests/src/test/java/com/epam/jdi/httptests/JSONPostTests.java
+++ jdi-dark-tests/src/test/java/com/epam/jdi/httptests/JSONPostTests.java
@@ -10,6 +10,10 @@ import org.testng.annotations.Test;
 import static com.epam.http.requests.RequestData.requestBody;
 import static com.epam.http.requests.RequestData.requestData;
 import static com.epam.http.requests.ServiceInit.init;
+import static com.epam.jdi.httptests.JettyService.greetPost;
+import static com.epam.jdi.httptests.JettyService.jsonBodyPost;
+import static com.epam.jdi.httptests.JettyService.notFoundedURIPost;
+import static com.epam.jdi.httptests.JettyService.unauthorizedPost;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.allOf;
@@ -20,97 +24,79 @@ import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 public class JSONPostTests {
     @BeforeClass
     public void initService() {
-        init(JettyApi.class);
+        init(JettyService.class);
     }
 
-    @Test(priority = 1)
+    @Test
     public void simpleJSONAndHamcrestMatcherJDI() {
-        RestResponse response = JettyApi.greetPost
-                .call(requestData(d -> {
-                    d.queryParams.add(new Object[][]{{"firstName", "John"}, {"lastName", "Doe"}});
-                }));
+        RestResponse response = greetPost
+                .call(requestData(d -> d.queryParams.add(new Object[][]{{"firstName", "John"}, {"lastName", "Doe"}})));
         response.isOk().body("greeting", equalTo("Greetings John Doe"));
     }
 
     @Test
     public void formParamsAcceptsIntArgumentsJDI() {
-        RestResponse response = JettyApi.greetPost
-                .call(requestData(d -> {
-                    d.queryParams.add(new Object[][]{{"firstName", 1234}, {"lastName", 5678}});
-                }));
+        RestResponse response = greetPost
+                .call(requestData(d -> d.queryParams.add(new Object[][]{{"firstName", 1234}, {"lastName", 5678}})));
         response.isOk().body("greeting", equalTo("Greetings 1234 5678"));
     }
 
-    @Test(priority = 2)
+    @Test
     public void bodyWithSingleHamcrestMatching() {
-        RestResponse response = JettyApi.greetPost
-                .call(requestData(d -> {
-                    d.queryParams.add(new Object[][]{{"firstName", 1234}, {"lastName", 5678}});
-                }));
+        RestResponse response = greetPost
+                .call(requestData(d -> d.queryParams.add(new Object[][]{{"firstName", 1234}, {"lastName", 5678}})));
         response.isOk().body(containsString("greeting"));
     }
 
     @Test
     public void bodyHamcrestMatcherWithoutKey() {
-        RestResponse response = JettyApi.greetPost
-                .call(requestData(d -> {
-                    d.queryParams.add(new Object[][]{{"firstName", "John"}, {"lastName", "Doe"}});
-                }));
+        RestResponse response = greetPost
+                .call(requestData(d -> d.queryParams.add(new Object[][]{{"firstName", "John"}, {"lastName", "Doe"}})));
         response.isOk().body(equalTo("{\"greeting\":\"Greetings John Doe\"}"));
-
     }
 
     @Test
     public void uriNotFoundTWhenPost() {
-        RestResponse response = JettyApi.notFoundedURI
-                .call();
+        RestResponse response = notFoundedURIPost.call();
         response.assertThat().statusCode(greaterThanOrEqualTo(400));
     }
 
     @Test
     public void requestSpecificationAllowsSpecifyingHeaders() {
-        RestResponse response = JettyApi.header
-                .call();
+        RestResponse response = JettyService.headerPost.call();
         response.isOk().body(containsString("MyHeader"));
     }
 
-
     @Test
     public void requestSpecificationAllowsSpecifyingJsonBodyForPost() {
-        RestResponse response = JettyApi.jsonBody
+        RestResponse response = jsonBodyPost
                 .call(requestBody("{ \"message\" : \"hello world\"}"));
-
         response.isOk().body(equalTo("hello world"));
         given().body("{ \"message\" : \"hello world\"}").with().contentType(ContentType.JSON).then().expect().body(equalTo("hello world")).when().post("/jsonBody");
     }
 
     @Test
     public void supportsReturningPostBody() {
-        RestResponse response = JettyApi.greetPost
-                .call(requestData(d -> {
-                    d.queryParams.add(new Object[][]{{"firstName", "John"}, {"lastName", "Doe"}});
-                }));
+        RestResponse response = greetPost
+                .call(requestData(d -> d.queryParams.add(new Object[][]{{"firstName", "John"}, {"lastName", "Doe"}})));
         final JsonPath jsonPath = new JsonPath(response.body);
         assertThat(jsonPath.getString("greeting"), equalTo("Greetings John Doe"));
     }
 
     @Test
     public void supportsGettingResponseBodyWhenStatusCodeIs401() {
-        RestResponse response = JettyApi.unathorizedRq.call();
+        RestResponse response = unauthorizedPost.call();
         assertThat(response.body, allOf(containsString("401"), containsString("Unauthorized")));
     }
 
     @Test
     public void requestSpecificationAllowsSpecifyingCookie() {
-        RestResponse response = JettyApi.cookie.call(requestData(requestData ->
+        RestResponse response = JettyService.cookiePost.call(requestData(requestData ->
                 requestData.cookies = new MapArray<>(new Object[][]{
                         {"username", "John"},
                         {"token", "1234"}
                 })));
         response.isOk()
                 .body(equalTo("username, token"));
-
     }
-
-
 }

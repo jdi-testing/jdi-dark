5271cfc542de364a0aa41043709682a37f402e89
TPetukhova <Tatiana_Petukhova@epam.com>
2020-01-30 16:09:45 +0300

    Some tests removed as they verify RA instead of Dark.

--- jdi-dark-tests/src/test/java/com/epam/jdi/httptests/ResponseTests.java
+++ jdi-dark-tests/src/test/java/com/epam/jdi/httptests/ResponseTests.java
@@ -2,26 +2,15 @@ package com.epam.jdi.httptests;
 
 import com.epam.http.response.RestResponse;
 import com.epam.jdi.tools.map.MapArray;
-import io.restassured.path.json.exception.JsonPathException;
-import io.restassured.path.xml.exception.XmlPathException;
-import org.apache.commons.io.IOUtils;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
 
 import static com.epam.http.requests.RequestData.requestBody;
 import static com.epam.http.requests.RequestData.requestData;
 import static com.epam.http.requests.ServiceInit.init;
-import static io.restassured.path.xml.XmlPath.CompatibilityMode.HTML;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.nullValue;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class ResponseTests {
@@ -94,12 +83,6 @@ public class ResponseTests {
     }
 
     @Test
-    public void whenNoExpectationsDefinedThenGetCanReturnBodyAsInputStream() throws IOException {
-        InputStream inputStream = JettyService.getHello.call().raResponse().asInputStream();
-        assertThat(IOUtils.toString(inputStream, Charset.defaultCharset()), equalTo("{\"hello\":\"Hello Scalatra\"}"));
-    }
-
-    @Test
     public void usingJsonPathViewFromTheResponse() {
         final String hello = JettyService.getHello.call().raResponse().jsonPath().getString("hello");
         assertThat(hello, equalTo("Hello Scalatra"));
@@ -109,123 +92,5 @@ public class ResponseTests {
     public void usingXmlPathViewFromTheResponse() {
         final String firstName = JettyService.postGreetXml.call(requestBody("firstName=John&lastName=Doe")).raResponse().xmlPath().getString("greeting.firstName");
         assertThat(firstName, equalTo("John"));
-    }
-
-    @Test
-    public void usingXmlPathWithHtmlCompatibilityModeFromTheResponse() {
-        final String title = JettyService.getTextHtml.call().raResponse().xmlPath(HTML).getString("html.head.title");
-        assertThat(title, equalTo("my title"));
-    }
-
-    @Test
-    public void usingHtmlPathToParseHtmlFromTheResponse() {
-        final String title = JettyService.getTextHtml.call().raResponse().htmlPath().getString("html.head.title");
-        assertThat(title, equalTo("my title"));
-    }
-
-    @Test
-    public void usingPathWithContentTypeJsonFromTheResponse() {
-        final String hello = JettyService.getHello.call().raResponse().path("hello");
-        assertThat(hello, equalTo("Hello Scalatra"));
-    }
-
-    @Test
-    public void usingPathWithParameters() {
-        final String hello = JettyService.getHello.call().raResponse().path("hel%s", "lo");
-        assertThat(hello, equalTo("Hello Scalatra"));
-    }
-
-    @Test
-    public void usingPathWithContentTypeXmlFromTheResponse() {
-        final String firstName = JettyService.postGreetXml.call(requestBody("firstName=John&lastName=Doe")).raResponse().path("greeting.firstName");
-        assertThat(firstName, equalTo("John"));
-    }
-
-    @Test
-    public void pathWorksForMultipleInvocationsWithJson() {
-        RestResponse response = JettyService.getJsonStore.call();
-        float minPrice = response.raResponse().path("store.book.price.min()");
-        float maxPrice = response.raResponse().path("store.book.price.max()");
-        assertThat(minPrice, is(8.95f));
-        assertThat(maxPrice, is(22.99f));
-    }
-
-    @Test(expectedExceptions = XmlPathException.class, expectedExceptionsMessageRegExp = "Failed to parse the XML document")
-    public void pathThrowsExceptionWhenTryingToUseXmlPathAfterHavingUsedJsonPath() {
-        RestResponse response = JettyService.getJsonStore.call();
-        response.raResponse().path("store.book.price.min()");
-        response.raResponse().xmlPath().get("store.book.price.min()");
-    }
-
-    @Test
-    public void pathWorksForMultipleInvocationsWithXml() {
-        RestResponse response = JettyService.getVideos.call();
-        String title = response.raResponse().path("videos.music[0].title.toString().trim()");
-        String artist = response.raResponse().path("videos.music[0].artist.toString().trim()");
-        assertThat(title, equalTo("Video Title 1"));
-        assertThat(artist, equalTo("Artist 1"));
-    }
-
-    @Test(expectedExceptions = JsonPathException.class, expectedExceptionsMessageRegExp = "Failed to parse the JSON document")
-    public void pathThrowsExceptionWhenTryingToUseJsonPathAfterHavingUsedXmlPath() {
-        RestResponse response = JettyService.getVideos.call();
-        response.raResponse().path("videos.music[0].title.toString().trim()");
-        response.raResponse().jsonPath().get("videos");
-    }
-
-    @Test
-    public void canParsePathAfterPrettyPrint() {
-        RestResponse response = JettyService.getVideos.call();
-        response.raResponse().prettyPrint();
-        String title = response.raResponse().path("videos.music[0].title.toString().trim()");
-        String artist = response.raResponse().path("videos.music[0].artist.toString().trim()");
-
-        assertThat(title, equalTo("Video Title 1"));
-        assertThat(artist, equalTo("Artist 1"));
-    }
-
-    @Test
-    public void canParsePathAfterPrint() {
-        RestResponse response = JettyService.getVideos.call();
-        response.raResponse().print();
-        String title = response.raResponse().path("videos.music[0].title.toString().trim()");
-        String artist = response.raResponse().path("videos.music[0].artist.toString().trim()");
-
-        assertThat(title, equalTo("Video Title 1"));
-        assertThat(artist, equalTo("Artist 1"));
-    }
-
-    @Test
-    public void canGetAsStringMultipleTimes() {
-        RestResponse response = JettyService.getVideos.call();
-        response.raResponse().asString();
-        String string = response.raResponse().asString();
-        assertThat(string, not(nullValue()));
-    }
-
-    @Test
-    public void canGetAsByteArrayMultipleTimes() {
-        RestResponse response = JettyService.getVideos.call();
-        response.raResponse().asByteArray();
-        final byte[] bytes = response.raResponse().asByteArray();
-        assertThat(bytes, not(nullValue()));
-    }
-
-    @Test
-    public void canCombineAsByteArrayWithPrettyPrintAndAsString() {
-        RestResponse response = JettyService.getVideos.call();
-        response.raResponse().asByteArray();
-        response.raResponse().prettyPrint();
-        String string = response.raResponse().asString();
-        assertThat(string, not(nullValue()));
-    }
-
-    @Test
-    public void canCombineAsStringWithPrettyPrintAndAsByteArray() {
-        RestResponse response = JettyService.getVideos.call();
-        response.raResponse().asString();
-        response.raResponse().prettyPrint();
-        byte[] bytes = response.raResponse().asByteArray();
-        assertThat(bytes, not(nullValue()));
     }
 }

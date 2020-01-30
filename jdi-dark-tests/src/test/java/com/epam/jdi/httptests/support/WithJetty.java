623f74ec1ffbedb186f7bb4573ec87bac0c11e0c
Andrei_Belousov <Andrei_Belousov@epam.com>
2020-01-30 14:26:33 +0300

    scalatra-webapp

--- jdi-dark-tests/src/test/java/com/epam/jdi/httptests/support/WithJetty.java
+++ jdi-dark-tests/src/test/java/com/epam/jdi/httptests/support/WithJetty.java
@@ -1,36 +1,16 @@
 package com.epam.jdi.httptests.support;
 
-import io.restassured.RestAssured;
-import org.eclipse.jetty.annotations.AnnotationConfiguration;
-import org.eclipse.jetty.http.HttpVersion;
-import org.eclipse.jetty.plus.webapp.EnvConfiguration;
-import org.eclipse.jetty.plus.webapp.PlusConfiguration;
-import org.eclipse.jetty.security.ConstraintMapping;
-import org.eclipse.jetty.security.ConstraintSecurityHandler;
-import org.eclipse.jetty.security.HashLoginService;
-import org.eclipse.jetty.security.LoginService;
-import org.eclipse.jetty.security.authentication.BasicAuthenticator;
 import org.eclipse.jetty.server.*;
-import org.eclipse.jetty.util.security.Constraint;
-import org.eclipse.jetty.util.ssl.SslContextFactory;
-import org.eclipse.jetty.webapp.*;
+import org.eclipse.jetty.webapp.WebAppContext;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
 
 import java.io.File;
-import java.util.Collections;
 
 public abstract class WithJetty {
-    public static final String itestPath;
     private static Server server;
-
-    static {
-        String fileSeparator = System.getProperty("file.separator");
-        itestPath = fileSeparator + "examples" + fileSeparator + "rest-assured-itest-java";
-    }
 
     @Rule
     public ExpectedException exception = ExpectedException.none();
@@ -49,28 +29,16 @@ public abstract class WithJetty {
         http.setIdleTimeout(30000);
 
         String warpath = "target/webapps/scalatra-webapp.war";
+        File warfile = new File(warpath);
 
         WebAppContext context = new WebAppContext();
-        context.setResourceBase(warpath);
-        context.setConfigurations(new Configuration[]
-                {
-                        new AnnotationConfiguration(),
-                        new WebInfConfiguration(),
-                        new WebXmlConfiguration(),
-                        new MetaInfConfiguration(),
-                        new FragmentConfiguration(),
-                        new EnvConfiguration(),
-                        new PlusConfiguration(),
-                        new JettyWebXmlConfiguration()
-                });
-
+        context.setWar(warfile.getAbsolutePath());
         context.setContextPath("/");
-//        context.setParentLoaderPriority(true);
+        context.setParentLoaderPriority(true);
         server.setHandler(context);
         server.setConnectors(new Connector[]{http});
         server.start();
     }
-
 
     @AfterClass
     public static void stopJetty() throws Exception {

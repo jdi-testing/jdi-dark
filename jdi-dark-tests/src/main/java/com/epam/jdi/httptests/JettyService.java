53de829917a27c66e736a1f3b617894188fe555f
iakovsidorov <ziforigin@gmail.com>
2020-01-30 17:43:35 +0300

    Header test are added, which consists of just 2 tests. All other checks are done in restassured and can onlu be done by restassured methods. Please refer to Jdi-dark missing capabilities doc.

--- jdi-dark-tests/src/main/java/com/epam/jdi/httptests/JettyService.java
+++ jdi-dark-tests/src/main/java/com/epam/jdi/httptests/JettyService.java
@@ -1,9 +1,14 @@
 package com.epam.jdi.httptests;
 
+import com.epam.http.annotations.ContentType;
 import com.epam.http.annotations.GET;
+import com.epam.http.annotations.Header;
+import com.epam.http.annotations.Headers;
 import com.epam.http.annotations.POST;
 import com.epam.http.annotations.ServiceDomain;
 import com.epam.http.requests.RestMethod;
+
+import static io.restassured.http.ContentType.JSON;
 
 @ServiceDomain("http://localhost:8080/")
 public class JettyService {
@@ -22,4 +27,15 @@ public class JettyService {
 
     @GET("/setCommonIdCookies")
     static RestMethod getCommonIdCookies;
+
+    @GET("/header")
+    @Header(name = "HeaderTestName", value = "HeaderTestValue")
+    static RestMethod getWithSingleHeaderInRequest;
+
+    @GET("/header")
+    @Headers({
+            @Header(name = "Header1", value = "Value1"),
+            @Header(name = "Header2", value = "Value2")
+    })
+    static RestMethod getWithMultipleHeadersInRequest;
 }

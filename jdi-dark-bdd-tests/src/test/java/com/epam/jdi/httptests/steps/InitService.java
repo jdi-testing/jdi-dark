4a002fa5d07a8e655ebfe6949f25f1aef1da9305
tsvetaev.alex <tsvetaev.alex@gmail.com>
2020-01-31 13:38:43 +0300

    Update InitService.java
    fix file missed from commit

--- jdi-dark-bdd-tests/src/test/java/com/epam/jdi/httptests/steps/InitService.java
+++ jdi-dark-bdd-tests/src/test/java/com/epam/jdi/httptests/steps/InitService.java
@@ -1,10 +1,15 @@
+import static com.epam.http.requests.ServiceInit.init;
+import static com.epam.jdi.http.Utils.domainUrl;
+import static com.epam.jdi.http.Utils.getDomain;
 package com.epam.jdi.httptests.steps;
 
 import com.epam.jdi.httptests.ServiceExample;
 import io.cucumber.java.en.Given;
-
 import static com.epam.http.requests.ServiceInit.init;
-import static com.epam.jdi.http.Utils.*;
+import static com.epam.jdi.http.Utils.domainUrl;
+import static com.epam.jdi.http.Utils.getDomain;
+
+
 
 public class InitService {
     @Given("^I init service$")

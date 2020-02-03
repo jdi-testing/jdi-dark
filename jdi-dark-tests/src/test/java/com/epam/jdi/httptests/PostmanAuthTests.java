ff2e0aecb7281fa71fa13dc01ec4586108ed56f8
Vitaly Dukhov <vitaly.dukhov@gmail.com>
2020-02-03 13:56:56 +0300

    fix auth tests

--- jdi-dark-tests/src/test/java/com/epam/jdi/httptests/PostmanAuthTests.java
+++ jdi-dark-tests/src/test/java/com/epam/jdi/httptests/PostmanAuthTests.java
@@ -44,7 +44,7 @@ public class PostmanAuthTests {
     @Test
     public void authPutCredentialsInFormTest() {
         RequestSpecification resp2 = authBaseForm
-                .getSpec()
+                .addRestSpecificationData()
                 .auth()
                 .basic("postman", "password");
         RestResponse resp = authBaseForm.call(resp2);
@@ -61,7 +61,7 @@ public class PostmanAuthTests {
 
     @Test
     public void authBaseFailTest() {
-        RequestSpecification spec = authBase.getSpec().header("Authorization", "Basic cG9zdG1hbjpwYXNzd29yBB==");
+        RequestSpecification spec = authBase.addRestSpecificationData().header("Authorization", "Basic cG9zdG1hbjpwYXNzd29yBB==");
         RestResponse resp = authBaseForm.call(spec);
         resp.assertThat()
                 .statusCode(HttpStatus.SC_UNAUTHORIZED);
@@ -69,7 +69,7 @@ public class PostmanAuthTests {
 
     @Test
     public void authDigestTest() {
-        RequestSpecification rs = authDigest.getSpec()
+        RequestSpecification rs = authDigest.addRestSpecificationData()
                 .auth().digest("postman", "password");
         RestResponse resp = authDigest.call(rs);
         resp.isOk().assertThat().

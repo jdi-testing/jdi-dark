a78d54c3185685967efe912e019378f0eec9fadb
Mariia_Tolstaia <Mariia_Tolstaia@epam.com>
2020-02-04 13:01:24 +0300

    add ContentType tests

--- jdi-dark-tests/src/main/java/com/epam/jdi/httptests/JettyService.java
+++ jdi-dark-tests/src/main/java/com/epam/jdi/httptests/JettyService.java
@@ -110,4 +110,14 @@ public class JettyService {
 
     @GET("/jsonStore")
     static RestMethod getJsonStore;
+
+    @GET("/contentTypeAsBody")
+    static RestMethod getContentTypeAsBody;
+
+    @POST("/return204WithContentType")
+    @ContentType(JSON)
+    static RestMethod postReturn204WithContentType;
+
+    @GET("/headersWithValues")
+    static RestMethod getHeadersWithValues;
 }

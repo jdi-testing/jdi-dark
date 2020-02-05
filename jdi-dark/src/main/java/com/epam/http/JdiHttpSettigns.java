ebc033b1b243a22cbc4413f609038548a7ae801d
Elena_Poduzova <Elena_Poduzova@epam.com>
2020-02-05 11:36:45 +0300

    Custom logger from jdi and loading settings from .properties file.

--- jdi-dark/src/main/java/com/epam/http/JdiHttpSettigns.java
+++ jdi-dark/src/main/java/com/epam/http/JdiHttpSettigns.java
@@ -1,13 +1,57 @@
 package com.epam.http;
 
-import org.slf4j.Logger;
+import com.epam.http.logger.ILogger;
+import com.epam.jdi.tools.PropertyReader;
 
-import static org.slf4j.LoggerFactory.getLogger;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import static com.epam.http.logger.HTTPLogger.instance;
+import static com.epam.http.logger.LogLevels.parseLogLevel;
+import static com.epam.jdi.tools.PathUtils.mergePath;
+import static com.epam.jdi.tools.PropertyReader.fillAction;
 
 /**
  * @author <a href="mailto:roman.iovlev.jdi@gmail.com">Roman_Iovlev</a>
  */
 public class JdiHttpSettigns {
+    public static String TEST_PROPERTIES_PATH = "test.properties";
     public static boolean verifyOkStatus = false;
-    public static Logger logger = getLogger("Http logger");
+    public static ILogger logger = instance("JDI");
+
+    public static synchronized void init() {
+        getProperties(TEST_PROPERTIES_PATH);
+        fillAction(p -> logger.setLogLevel(parseLogLevel(p)), "log.level");
+    }
+
+    private static Properties getProperties(String path) {
+        File propertyFile = new File(path);
+        Properties properties;
+        if (propertyFile.exists()) {
+            properties = getCiProperties(path, propertyFile);
+        } else {
+            Properties pTest = PropertyReader.getProperties(mergePath(path));
+            Properties pTarget = PropertyReader.getProperties(mergePath("/../../target/classes/" + path));
+            if (pTarget.size() > 0)
+                return pTarget;
+            String propertiesPath = pTest.size() > 0
+                    ? path
+                    : mergePath("/../../target/classes/" + path);
+            properties = PropertyReader.getProperties(propertiesPath);
+        }
+        return properties;
+    }
+
+    private static Properties getCiProperties(String path, File propertyFile) {
+        Properties properties = new Properties();
+        try {
+            System.out.println("Property file found: " + propertyFile.getAbsolutePath());
+            properties.load(new FileInputStream(propertyFile));
+        } catch (IOException ex) {
+            throw new RuntimeException("Couldn't load properties for CI Server" + path);
+        }
+        return properties;
+    }
 }
---
+++ jdi-dark/src/main/java/com/epam/http/logger/HTTPLogger.java
@@ -0,0 +1,149 @@
+package com.epam.http.logger;
+
+import com.epam.jdi.tools.Safe;
+import com.epam.jdi.tools.func.JAction;
+import com.epam.jdi.tools.func.JFunc;
+import com.epam.jdi.tools.map.MapArray;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.epam.http.logger.LogLevels.DEBUG;
+import static com.epam.http.logger.LogLevels.INFO;
+import static com.epam.http.logger.LogLevels.OFF;
+import static com.epam.http.logger.LogLevels.STEP;
+import static com.epam.http.logger.LogLevels.TRACE;
+import static com.epam.http.logger.LogLevels.getLog4j2Level;
+import static com.epam.jdi.tools.StringUtils.format;
+import static org.apache.logging.log4j.LogManager.getLogger;
+import static org.apache.logging.log4j.core.config.Configurator.setLevel;
+import static org.apache.logging.log4j.core.config.Configurator.setRootLevel;
+
+/**
+ * Created by Roman Iovlev on 14.02.2018
+ * Email: roman.iovlev.jdi@gmail.com; Skype: roman.iovlev
+ */
+public class HTTPLogger implements ILogger {
+    private static MapArray<String, HTTPLogger> loggers = new MapArray<>();
+    private static Marker jdiMarker = MarkerManager.getMarker("JDI");
+
+    public static HTTPLogger instance(String name) {
+        if (!loggers.has(name))
+            loggers.add(name, new HTTPLogger(name));
+        return loggers.get(name);
+    }
+
+    public HTTPLogger() {
+        this("JDI");
+    }
+    public HTTPLogger(String name) {
+        logger = getLogger(name);
+        this.name = name;
+        setLogLevel(INFO);
+    }
+    public HTTPLogger(Class clazz) {
+        this(clazz.getSimpleName());
+    }
+
+    private Safe<LogLevels> logLevel = new Safe<>(INFO);
+
+    public LogLevels getLogLevel() {
+        return logLevel.get();
+    }
+    public void setLogLevel(LogLevels level) {
+        logLevel = new Safe<>(level);
+        setRootLevel(getLog4j2Level(level));
+        setLevel(name, getLog4j2Level(level));
+    }
+    private Safe<Integer> logOffDeepness = new Safe<>(0);
+
+    public void logOff() {
+        logLevel.set(OFF);
+        logOffDeepness.update(v->v+1);
+    }
+    public void logOn() {
+        logOffDeepness.update(v->v-1);
+        if (logOffDeepness.get() > 0) return;
+        if (logOffDeepness.get() == 0)
+            logLevel.reset();
+        if (logOffDeepness.get() < 0)
+            throw new RuntimeException("Log Off Deepness to high. Please check that each logOff has appropriate logOn");
+    }
+    public void dropLogOff() {
+        logOffDeepness.set(0);
+        logLevel.reset();
+    }
+    public void logOff(JAction action) {
+        logOff(() -> { action.invoke(); return null; });
+    }
+    public <T> T logOff(JFunc<T> func) {
+        LogLevels tempLevel = logLevel.get();
+        if (logLevel.get() == OFF) {
+            try { return func.invoke();
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        logLevel.set(OFF);
+        T result;
+        try{ result = func.invoke(); }
+        catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+        logLevel.set(tempLevel);
+        return result;
+    }
+    private String name;
+    private Logger logger;
+    private String getRecord(String record, Object... args) {
+        return format(record, args);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void step(String s, Object... args) {
+        if (logLevel.get().equalOrLessThan(STEP)) {
+            logger.log(Level.forName("STEP", 350), jdiMarker, getRecord(s, args));
+        }
+    }
+
+    public void trace(String s, Object... args) {
+        if (logLevel.get().equalOrLessThan(TRACE)) {
+            logger.trace(jdiMarker, getRecord(s, args));
+        }
+    }
+    public void debug(String s, Object... args) {
+        if (logLevel.get().equalOrLessThan(DEBUG)) {
+            logger.debug(jdiMarker, getRecord(s, args));
+        }
+    }
+    public void info(String s, Object... args) {
+        if (logLevel.get().equalOrLessThan(INFO)) {
+            logger.info(jdiMarker, getRecord(s, args));
+        }
+    }
+    public void error(String s, Object... args) {
+        logger.error(jdiMarker, getRecord(s, args));
+    }
+
+    public void toLog(String msg) {
+        toLog(msg, logLevel.getDefault());
+    }
+    public void toLog(String msg, LogLevels level) {
+        if (logLevel.get().equalOrLessThan(level))
+            switch (level) {
+                case ERROR: error(msg); break;
+                case STEP: step(msg); break;
+                case INFO: info(msg); break;
+                case DEBUG: debug(msg); break;
+                case OFF: break;
+                default: throw new RuntimeException("Unknown log level: " + level);
+            }
+    }
+}

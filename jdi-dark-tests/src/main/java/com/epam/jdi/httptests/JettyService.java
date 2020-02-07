99831f80ad839a3cc776fad51ed85a23425d0b67
iakovsidorov <ziforigin@gmail.com>
2020-02-07 12:49:53 +0300

    small fix for accidentally removed jetty service variables

--- jdi-dark-tests/src/main/java/com/epam/jdi/httptests/JettyService.java
+++ jdi-dark-tests/src/main/java/com/epam/jdi/httptests/JettyService.java
@@ -127,4 +127,16 @@ public class JettyService {
     @GET("/redirect")
     public static RestMethod postRedirect;
 
+    @GET("/{firstName}/{lastName}")
+    public static RestMethod getUser;
+
+    @GET("/{path}.json")
+    public static RestMethod getParamBeforePath;
+
+    @GET("/something.{format}")
+    public static RestMethod getParamAfterPath;
+
+    @GET("/matrix;{abcde}={value}")
+    public static RestMethod getMatrix;
+
 }

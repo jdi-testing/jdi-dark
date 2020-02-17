f301caa82a86f43ff0bde48a56d64dfd44cb03dc
Elena_Poduzova <Elena_Poduzova@epam.com>
2020-02-17 11:22:22 +0300

    Fix log.

--- jdi-dark/src/main/java/com/epam/http/JdiHttpSettigns.java
+++ jdi-dark/src/main/java/com/epam/http/JdiHttpSettigns.java
@@ -60,7 +60,7 @@ public class JdiHttpSettigns {
     }
 
     private static Properties getProperties(String path) {
-        logger.info("Properties file is: " + path + ".properties");
+        logger.info("Properties file is: " + path);
         File propertyFile = new File(path);
         Properties properties;
         if (propertyFile.exists()) {

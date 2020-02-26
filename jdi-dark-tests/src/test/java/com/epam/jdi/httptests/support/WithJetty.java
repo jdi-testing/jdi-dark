c13dae86684c9ada45cbbaf6d7f75f5e1808e15a
Mariia_Tolstaia <Mariia_Tolstaia@epam.com>
2020-02-26 13:24:48 +0300

    update WithJetty class, add https version

--- jdi-dark-tests/src/test/java/com/epam/jdi/httptests/support/WithJetty.java
+++ jdi-dark-tests/src/test/java/com/epam/jdi/httptests/support/WithJetty.java
@@ -1,21 +1,31 @@
 package com.epam.jdi.httptests.support;
 
+import org.eclipse.jetty.http.HttpVersion;
+import org.eclipse.jetty.security.ConstraintMapping;
+import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.security.HashLoginService;
+import org.eclipse.jetty.security.LoginService;
+import org.eclipse.jetty.security.authentication.BasicAuthenticator;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.HttpConfiguration;
 import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.SecureRequestCustomizer;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.SslConnectionFactory;
+import org.eclipse.jetty.util.security.Constraint;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
-import java.io.File;
+import java.util.Collections;
 
 public abstract class WithJetty {
     private static Server server;
 
     @BeforeClass
-    public static void startJetty() throws Exception {
+    public static void startJettyHttps() throws Exception {
         server = new Server();
 
         HttpConfiguration httpConfig = new HttpConfiguration();
@@ -25,19 +35,53 @@ public abstract class WithJetty {
 
         ServerConnector http = new ServerConnector(server, new HttpConnectionFactory(httpConfig));
         http.setPort(8080);
-        http.setIdleTimeout(30000);
+        http.setIdleTimeout(300000);
 
-        String warpath = "target/webapps/scalatra-webapp.war";
-        File warfile = new File(warpath);
+        HttpConfiguration httpsConfig = new HttpConfiguration(httpConfig);
+        httpsConfig.addCustomizer(new SecureRequestCustomizer());
+
+        String file = "src/test/resources/jetty_localhost_server.jks";
+        SslContextFactory.Server sslContextFactory = new SslContextFactory.Server();
+        sslContextFactory.setKeyStorePath(file);
+        sslContextFactory.setKeyStorePassword("test1234");
+
+        ServerConnector https = new ServerConnector(server,
+                new SslConnectionFactory(sslContextFactory, HttpVersion.HTTP_1_1.asString()),
+                new HttpConnectionFactory(httpsConfig));
+        https.setPort(8443);
+        https.setIdleTimeout(500000);
+
+        // Security config
+        Constraint constraint = new Constraint();
+        constraint.setName(Constraint.__BASIC_AUTH);
+        constraint.setRoles(new String[]{"user", "admin", "moderator"});
+        constraint.setAuthenticate(true);
+
+        ConstraintMapping mapping = new ConstraintMapping();
+        mapping.setConstraint(constraint);
+        mapping.setPathSpec("/secured/*");
+
+        LoginService loginService = new HashLoginService("MyRealm", "src/test/resources/realm.properties");
+        server.addBean(loginService);
+
+        ConstraintSecurityHandler security = new ConstraintSecurityHandler();
+        server.setHandler(security);
+        security.setConstraintMappings(Collections.singletonList(mapping));
+        security.setAuthenticator(new BasicAuthenticator());
+        security.setLoginService(loginService);
+
+        // End security config
 
         WebAppContext context = new WebAppContext();
-        context.setWar(warfile.getAbsolutePath());
         context.setContextPath("/");
-        context.setParentLoaderPriority(true);
-        server.setHandler(context);
-        server.setConnectors(new Connector[]{http});
+        String warPath = "target/webapps/scalatra-webapp.war";
+        context.setWar(warPath);
+        context.setServer(server);
+
+        security.setHandler(context);
+        server.setHandler(security);
+        server.setConnectors(new Connector[]{http, https});
         server.start();
-        
     }
 
     @AfterClass
@@ -45,5 +89,4 @@ public abstract class WithJetty {
         server.stop();
         server.join();
     }
-
 }

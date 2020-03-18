a6e37e230c45075eb537f2086ebc5edc424fddb3
Yulia Litvin <yulia_litvin@epam.com>
2020-03-18 14:06:35 +0300

    parallel test classes

---
+++ jdi-dark-tests/src/test/java/com/epam/jdi/httptests/ParallelTest2.java
@@ -0,0 +1,33 @@
+package com.epam.jdi.httptests;
+
+import com.epam.http.response.RestResponse;
+import com.epam.jdi.httptests.support.WithJetty;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.epam.http.requests.ServiceInit.init;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ParallelTest2 extends WithJetty {
+    JettyService jetty;
+
+    @BeforeClass
+    public void before() {
+        jetty = init(JettyService.class);
+    }
+
+//    @Test
+//    public void canSpecifyMultiValueCookiesPassingSeveralValuesToTheCookieMethod() {
+//        RestResponse response = jetty.getMultiCookieWithOneName("key1", "yo", "yo2");
+//        assertThat(response.getBody(), equalTo("[{\"key1\":\"yo\"},{\"key1\":\"yo2\"}]"));
+//    }
+
+    @Test
+    public void canPassCookiesAsObjectsArray() {
+        Object[][] cookiesArray = new Object[][]{{"key1", "value1"}, {"key2", "value2"}};
+        RestResponse response = jetty.getMultiCookiesArray(cookiesArray);
+        assertThat(response.getBody(), equalTo("[{\"key1\":\"value1\"},{\"key2\":\"value2\"}]"));
+    }
+
+}

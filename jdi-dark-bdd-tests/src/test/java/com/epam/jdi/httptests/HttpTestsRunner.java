61842aef378e9279e034e546626c4d9323d8e42d
Roman Iovlev <roman_iovlev@epam.com>
2020-03-31 00:07:23 +0300

    correct bdd

--- jdi-dark-bdd-tests/src/test/java/com/epam/jdi/httptests/HttpTestsRunner.java
+++ jdi-dark-bdd-tests/src/test/java/com/epam/jdi/httptests/HttpTestsRunner.java
@@ -3,7 +3,8 @@ package com.epam.jdi.httptests;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/features/",
+@CucumberOptions(
+        features = "src/test/resources/features/",
         glue = {"com/epam/jdi/httptests/steps", "com/epam/jdi/http/stepdefs/en"},
         tags = {"@smoke"})
 public class HttpTestsRunner extends AbstractTestNGCucumberTests {

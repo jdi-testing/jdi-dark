1b50c2d1a13f8d8135548e4b98f8e4873ffbb4af
tsvetaevAlex <56589282+tsvetaevAlex@users.noreply.github.com>
2020-04-03 12:15:35 +0300

    negative test status 400

---
+++ jdi-dark-tests/src/test/java/com/epam/jdi/httptests/examples/requestparams/negativeTests.java
@@ -0,0 +1,32 @@
+package com.epam.jdi.httptests.examples.requestparams;
+
+
+import com.epam.http.response.RestResponse;
+import com.epam.jdi.httptests.JettyService;
+import com.epam.jdi.httptests.support.WithJetty;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+import static com.epam.http.requests.ServiceInit.init;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class negativeTests extends WithJetty {
+
+
+    @BeforeClass
+    public void before() {
+        init(JettyService.class);
+    }
+
+    @Test
+    public void brokenStringTest(){
+        RestResponse response = JettyService.getMultiCookieWithOneName("key`1", "value\1", "value2\n");
+        assertThat(response.getStatus().code, equalTo(400));
+        assertThat(response.getBody(), containsString("<h1>Bad Message 400</h1><pre>reason: Illegal character"));
+    }
+}
+
+

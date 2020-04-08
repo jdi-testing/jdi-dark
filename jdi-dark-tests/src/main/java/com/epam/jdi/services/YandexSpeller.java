506193ad2bfb18c8d2425a7500259a9b6f581fa3
Andrei Belousov <Andrei_Belousov@epam.com>
2020-04-08 14:20:48 +0300

    new method

--- jdi-dark-tests/src/main/java/com/epam/jdi/services/YandexSpeller.java
+++ jdi-dark-tests/src/main/java/com/epam/jdi/services/YandexSpeller.java
@@ -4,6 +4,8 @@ import com.epam.http.annotations.*;
 import com.epam.http.requests.SoapMethod;
 import com.epam.jdi.dto.yandex.speller.services.spellservice.CheckTextRequest;
 import com.epam.jdi.dto.yandex.speller.services.spellservice.CheckTextResponse;
+import com.epam.jdi.dto.yandex.speller.services.spellservice.CheckTextsRequest;
+import com.epam.jdi.dto.yandex.speller.services.spellservice.CheckTextsResponse;
 
 @ServiceDomain("http://speller.yandex.net/services/spellservice")
 public class YandexSpeller {
@@ -11,5 +13,9 @@ public class YandexSpeller {
     @POST("")
     @Header(name = "Content-Type", value = "text/xml;charset=UTF-8")
     public static SoapMethod<CheckTextRequest, CheckTextResponse> checkText;
+
+    @POST("")
+    @Header(name = "Content-Type", value = "text/xml;charset=UTF-8")
+    public static SoapMethod<CheckTextsRequest, CheckTextsResponse> checkTexts;
 
 }

d71c2ca7a74c578308204c722e7b91c8a01cef1c
Andrei Belousov <Andrei_Belousov@epam.com>
2020-05-07 19:14:50 +0300

    fix

--- jdi-dark-ws/src/main/java/com/epam/jdi/http/JdiWSServer.java
+++ jdi-dark-ws/src/main/java/com/epam/jdi/http/JdiWSServer.java
@@ -1,5 +1,6 @@
 package com.epam.jdi.http;
 
+import com.epam.http.logger.ILogger;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
@@ -10,8 +11,11 @@ import java.net.URISyntaxException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import static com.epam.http.logger.HTTPLogger.instance;
+
 public class JdiWSServer extends Endpoint {
 
+    public static ILogger logger = instance("JDI_WS");
     public WebSocketContainer webSocketContainer = ContainerProvider.getWebSocketContainer();
     public CountDownLatch latch = new CountDownLatch(1);
     public JsonElement receivedMessage;
@@ -22,6 +26,7 @@ public class JdiWSServer extends Endpoint {
     }
 
     public void connect(String path) throws IOException, DeploymentException, URISyntaxException {
+        logger.info("Connect to: " + path);
         connect(new URI(path));
     }
 
@@ -31,7 +36,7 @@ public class JdiWSServer extends Endpoint {
         this.session.getBasicRemote();
         this.session.addMessageHandler(new MessageHandler.Whole<String>() {
             public void onMessage(String s) {
-                System.out.println("Get message:\n" + s);
+                logger.info("Get message:\n" + s);
                 receivedMessage = JsonParser.parseString(s);
                 latch.countDown();
             }
@@ -39,6 +44,7 @@ public class JdiWSServer extends Endpoint {
     }
 
     public void sendMessage(String s) throws IOException {
+        logger.info("Send message:\n" + s);
         receivedMessage = null;
         session.getBasicRemote().sendText(s);
     }

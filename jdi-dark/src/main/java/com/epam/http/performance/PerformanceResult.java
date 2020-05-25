0d90adf25287995ea4d66a4ed15b6270faf5aa33
Andrei Belousov <Andrei_Belousov@epam.com>
2020-05-25 17:33:53 +0300

    add min, max results

--- jdi-dark/src/main/java/com/epam/http/performance/PerformanceResult.java
+++ jdi-dark/src/main/java/com/epam/http/performance/PerformanceResult.java
@@ -4,9 +4,13 @@ import lombok.Data;
 
 import java.util.List;
 
+import static com.epam.http.JdiHttpSettings.logger;
+
 @Data
 public class PerformanceResult {
 
+    public long minResponseTime = 0;
+    public long maxResponseTime = 0;
     public long averageResponseTime = 0;
     public long numberOfRequests = 0;
     public long numberOfFails = 0;
@@ -24,6 +28,8 @@ public class PerformanceResult {
      */
     public void aggregateResult(List<ThreadResult> results) {
         results.forEach(res -> {
+            minResponseTime = minResponseTime == 0 ? res.getMinResponseTime() : Math.min(minResponseTime, res.getMinResponseTime());
+            maxResponseTime = Math.max(maxResponseTime, res.getMaxResponseTime());
             averageResponseTime = (averageResponseTime * numberOfRequests + res.getAverageResponseTime())
                     / (numberOfRequests + 1);
             numberOfRequests += res.getNumberOfRequests();
@@ -31,5 +37,16 @@ public class PerformanceResult {
             numberOfServerFails += res.getNumberOfServerFails();
             numberOfFails = numberOfClientFails + numberOfServerFails;
         });
+        logger.info("Performance test results:");
+        logger.info("Threads count: " + results.size());
+        logger.info("Requests count: " + numberOfRequests);
+        logger.info("Average response time: " + averageResponseTime);
+        logger.info("Minimum response time: " + minResponseTime);
+        logger.info("Maximum response time: " + maxResponseTime);
+        if (!noFails()) {
+            logger.info("Fails count: " + numberOfFails);
+            logger.info("Client fails count: " + numberOfClientFails);
+            logger.info("Server fails count: " + numberOfServerFails);
+        }
     }
 }

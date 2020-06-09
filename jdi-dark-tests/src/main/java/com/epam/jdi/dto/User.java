3256493e9cc6af5c0e48779629530d194cfe9492
Mikhail <32819223+mbystrovEPAM@users.noreply.github.com>
2020-06-09 12:32:32 +0300

    Codacy issue has been mitigated

--- jdi-dark-tests/src/main/java/com/epam/jdi/dto/User.java
+++ jdi-dark-tests/src/main/java/com/epam/jdi/dto/User.java
@@ -3,5 +3,7 @@ package com.epam.jdi.dto;
 import com.epam.jdi.tools.DataClass;
 
 public class User extends DataClass<User> {
-    public String firstName, lastName, fullName;
+    public String firstName;
+    public String lastName;
+    public String fullName;
 }

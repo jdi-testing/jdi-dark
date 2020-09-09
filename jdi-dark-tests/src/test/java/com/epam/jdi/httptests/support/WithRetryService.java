146431204bc0b8948a144cff9a3fd0131e2e52e1
Akio Nkodia <AKIONK@ext.inditex.com>
2020-09-09 15:16:20 +0300

    Add logger to startSpringBootApplication for debug

--- jdi-dark-tests/src/test/java/com/epam/jdi/httptests/support/WithRetryService.java
+++ jdi-dark-tests/src/test/java/com/epam/jdi/httptests/support/WithRetryService.java
@@ -1,12 +1,16 @@
 package com.epam.jdi.httptests.support;
 
+import com.epam.http.logger.ILogger;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import static com.epam.http.logger.HTTPLogger.instance;
+
 public abstract class WithRetryService {
+    public static ILogger logger = instance("JDI_WithRetryService");
 
     protected Process application;
 
@@ -14,8 +18,9 @@ public abstract class WithRetryService {
     public void startSpringBootApplication() throws IOException, InterruptedException {
         final String startRetryServiceCommand = "java -jar src/test/resources/jdi-dark-retry-service.jar";
         application = Runtime.getRuntime().exec(startRetryServiceCommand);
-        for (int i = 0; i < 5000 && !application.isAlive(); ++i) {
-            TimeUnit.MILLISECONDS.sleep(500);
+        for (int i = 0; i < 60 && !application.isAlive(); ++i) {
+            TimeUnit.SECONDS.sleep(10);
+            logger.info("Waiting for Retry Service to start: " + i);
         }
     }
 

04519a370e1a8d9d23e0d3f1369ad860bf3a3224
Andrei Belousov <Andrei_Belousov@epam.com>
2020-09-10 01:01:14 +0300

    use log file

--- jdi-dark-tests/src/test/java/com/epam/jdi/httptests/support/WithRetryService.java
+++ jdi-dark-tests/src/test/java/com/epam/jdi/httptests/support/WithRetryService.java
@@ -1,10 +1,13 @@
 package com.epam.jdi.httptests.support;
 
 import com.epam.http.logger.ILogger;
+import org.apache.commons.io.FileUtils;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.concurrent.TimeUnit;
 
 import static com.epam.http.logger.HTTPLogger.instance;
@@ -16,13 +19,15 @@ public abstract class WithRetryService {
 
     @BeforeClass
     public void startSpringBootApplication() throws IOException, InterruptedException {
-        final String startRetryServiceCommand = "java -jar src/test/resources/jdi-dark-retry-service.jar";
-        application = Runtime.getRuntime().exec(startRetryServiceCommand);
+        ProcessBuilder builder = new ProcessBuilder("java", "-jar", "src/test/resources/jdi-dark-retry-service.jar");
+        File logFile = new File(System.getProperty("user.dir") + "\\target\\log.txt");
+        builder.redirectOutput(logFile);
+        application = builder.start();
         logger.info("Inside startSpringBootApplication(): " + application.getOutputStream().toString());
-        TimeUnit.SECONDS.sleep(10);
-        for (int i = 0; i < 60 && !application.isAlive(); ++i) {
-            TimeUnit.SECONDS.sleep(10);
-            logger.info("Waiting for Retry Service to start: " + i);
+        String log = "";
+        while (application.isAlive() && !log.contains("Started App")) {
+            TimeUnit.SECONDS.sleep(1);
+            log = FileUtils.readFileToString(logFile, Charset.defaultCharset());
         }
     }
 

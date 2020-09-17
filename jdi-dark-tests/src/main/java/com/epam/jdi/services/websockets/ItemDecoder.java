1bca9a76305576c1348769a76f6768be005e549b
Konstantin Safutin <Konstantin_Safutin@epam.com>
2020-09-17 13:06:22 +0400

    style fix

--- jdi-dark-tests/src/main/java/com/epam/jdi/services/websockets/ItemDecoder.java
+++ jdi-dark-tests/src/main/java/com/epam/jdi/services/websockets/ItemDecoder.java
@@ -1,5 +1,6 @@
 package com.epam.jdi.services.websockets;
 
+import com.epam.http.logger.ILogger;
 import com.epam.jdi.dto.Item;
 import com.google.gson.Gson;
 
@@ -7,25 +8,31 @@ import javax.websocket.DecodeException;
 import javax.websocket.Decoder;
 import javax.websocket.EndpointConfig;
 
-public class ItemDecoder implements Decoder.Text<Item> {
+import static com.epam.http.logger.HTTPLogger.instance;
 
+public class ItemDecoder implements Decoder.Text<Item> {
+    private static final ILogger logger = instance("JDI_WS_Decoder");
     private static final Gson gson = new Gson();
 
     @Override
-    public Item decode(String s) throws DecodeException {
+    public Item decode(String s) {
+        logger.info("Decoding message");
         return gson.fromJson(s, Item.class);
     }
 
     @Override
     public boolean willDecode(String s) {
+        logger.info("Check if message will decode");
         return (s != null);
     }
 
     @Override
     public void init(EndpointConfig endpointConfig) {
+        logger.info("Initiate decoder");
     }
 
     @Override
     public void destroy() {
+        logger.info("Destroy decoder");
     }
 }
--- jdi-dark-tests/src/main/java/com/epam/jdi/services/websockets/ItemEncoder.java
+++ jdi-dark-tests/src/main/java/com/epam/jdi/services/websockets/ItemEncoder.java
@@ -1,5 +1,6 @@
 package com.epam.jdi.services.websockets;
 
+import com.epam.http.logger.ILogger;
 import com.epam.jdi.dto.Item;
 import com.google.gson.Gson;
 
@@ -7,20 +8,25 @@ import javax.websocket.EncodeException;
 import javax.websocket.Encoder;
 import javax.websocket.EndpointConfig;
 
-public class ItemEncoder implements Encoder.Text<Item> {
+import static com.epam.http.logger.HTTPLogger.instance;
 
+public class ItemEncoder implements Encoder.Text<Item> {
+    private static final ILogger logger = instance("JDI_WS_Encoder");
     private static final Gson gson = new Gson();
 
     @Override
-    public String encode(Item item) throws EncodeException {
+    public String encode(Item item) {
+        logger.info("Encoding message");
         return gson.toJson(item);
     }
 
     @Override
     public void init(EndpointConfig endpointConfig) {
+        logger.info("Initiate encoder");
     }
 
     @Override
     public void destroy() {
+        logger.info("Destroy encoder");
     }
 }

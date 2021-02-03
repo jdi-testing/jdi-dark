f4296594f18592b3a88262b68f80c98432c94686
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2021-02-03 19:56:02 +0300

    +SkipException

--- jdi-dark-tests/src/test/java/com/epam/jdi/httptests/examples/soap/DneOnlineCalculatorTests.java
+++ jdi-dark-tests/src/test/java/com/epam/jdi/httptests/examples/soap/DneOnlineCalculatorTests.java
@@ -3,38 +3,60 @@ package com.epam.jdi.httptests.examples.soap;
 import com.epam.jdi.soap.DneOnlineCalculator;
 import com.epam.jdi.soap.org.tempuri.*;
 import org.assertj.core.api.Assertions;
+import org.testng.SkipException;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.net.ConnectException;
 
 import static com.epam.http.requests.ServiceInit.init;
 
 public class DneOnlineCalculatorTests {
+    private boolean isSkipped = false;
+    private static final String skipMessage = "External Server is unavailable. Skip test";
 
     @BeforeClass
     public void before() {
         init(DneOnlineCalculator.class);
     }
 
-    @Test(skipFailedInvocations = true)
+    @Test()
     public void checkAdd() {
-        AddResponse response = DneOnlineCalculator.add.callSoap(new Add().withIntA(2).withIntB(3));
-        Assertions.assertThat(response.getAddResult()).isEqualTo(5);
+        try {
+            AddResponse response = DneOnlineCalculator.add.callSoap(new Add().withIntA(2).withIntB(3));
+            Assertions.assertThat(response.getAddResult()).isEqualTo(5);
+        }
+        catch (RuntimeException ex) {
+            if (ex.getCause() instanceof ConnectException) {
+                isSkipped = true;
+                throw new SkipException(skipMessage);
+            }
+        }
     }
 
-    @Test(skipFailedInvocations = true)
+    @Test()
     public void checkDivide() {
+        if (isSkipped) {
+            throw new SkipException(skipMessage);
+        }
         DivideResponse response = DneOnlineCalculator.divide.callSoap(new Divide().withIntA(6).withIntB(3));
         Assertions.assertThat(response.getDivideResult()).isEqualTo(2);
     }
 
-    @Test(skipFailedInvocations = true)
+    @Test()
     public void checkMultiply() {
+        if (isSkipped) {
+            throw new SkipException(skipMessage);
+        }
         MultiplyResponse response = DneOnlineCalculator.multiply.callSoap(new Multiply().withIntA(2).withIntB(3));
         Assertions.assertThat(response.getMultiplyResult()).isEqualTo(6);
     }
 
-    @Test(skipFailedInvocations = true)
+    @Test()
     public void checkSubtract() {
+        if (isSkipped) {
+            throw new SkipException(skipMessage);
+        }
         SubtractResponse response = DneOnlineCalculator.subtract.callSoap(new Subtract().withIntA(7).withIntB(3));
         Assertions.assertThat(response.getSubtractResult()).isEqualTo(4);
     }

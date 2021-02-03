f9d057e1f2f37dfae63c820fba44f4dd02833253
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2021-02-03 16:27:17 +0300

    Change retry condition

--- jdi-dark-tests/src/main/java/com/epam/jdi/utils/RetryAnalyzer.java
+++ jdi-dark-tests/src/main/java/com/epam/jdi/utils/RetryAnalyzer.java
@@ -5,6 +5,8 @@ import org.testng.ITestResult;
 
 import java.net.ConnectException;
 
+import static com.epam.http.JdiHttpSettings.logger;
+
 public class RetryAnalyzer implements IRetryAnalyzer {
 
     int counter = 0;
@@ -12,8 +14,10 @@ public class RetryAnalyzer implements IRetryAnalyzer {
 
     @Override
     public boolean retry(ITestResult result) {
-
-        if ((counter < retryLimit) && (result.getThrowable() instanceof ConnectException))
+        logger.info("Test %s failed %d times with error %s (%s)",
+                result.getName(), counter,
+                result.getThrowable().getClass().getName(), result.getThrowable().getMessage());
+        if ((counter < retryLimit))
         {
             counter++;
             return true;

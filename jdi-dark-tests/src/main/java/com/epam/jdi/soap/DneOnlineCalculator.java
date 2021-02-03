ed52dac9c6d902dc08955aa1d1b923773fa8b005
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2021-02-03 15:22:32 +0300

    +retry for soap

--- jdi-dark-tests/src/main/java/com/epam/jdi/soap/DneOnlineCalculator.java
+++ jdi-dark-tests/src/main/java/com/epam/jdi/soap/DneOnlineCalculator.java
@@ -4,25 +4,32 @@ import com.epam.http.annotations.*;
 import com.epam.http.requests.SoapMethod;
 import com.epam.jdi.soap.org.tempuri.*;
 
+import java.util.concurrent.TimeUnit;
+
+@RetryOnFailure
 @ServiceDomain("http://www.dneonline.com/calculator.asmx")
 @SOAPNamespace("http://tempuri.org/")
 public class DneOnlineCalculator {
 
     @POST
     @SOAPAction("http://tempuri.org/Add")
+    @RetryOnFailure(numberOfRetryAttempts = 5, delay = 1, unit = TimeUnit.SECONDS)
     public static SoapMethod<Add, AddResponse> add;
 
     @POST
     @SOAPAction("http://tempuri.org/Divide")
+    @RetryOnFailure(numberOfRetryAttempts = 5, delay = 1, unit = TimeUnit.SECONDS)
     public static SoapMethod<Divide, DivideResponse> divide;
 
     @POST
     @SOAPAction("http://tempuri.org/Multiply")
     @SOAP12
+    @RetryOnFailure(numberOfRetryAttempts = 5, delay = 1, unit = TimeUnit.SECONDS)
     public static SoapMethod<Multiply, MultiplyResponse> multiply;
 
     @POST
     @SOAPAction("http://tempuri.org/Subtract")
     @SOAP12
+    @RetryOnFailure(numberOfRetryAttempts = 5, delay = 1, unit = TimeUnit.SECONDS)
     public static SoapMethod<Subtract, SubtractResponse> subtract;
 }
--- jdi-dark-tests/src/test/java/com/epam/jdi/httptests/examples/soap/DneOnlineCalculatorTests.java
+++ jdi-dark-tests/src/test/java/com/epam/jdi/httptests/examples/soap/DneOnlineCalculatorTests.java
@@ -2,13 +2,27 @@ package com.epam.jdi.httptests.examples.soap;
 
 import com.epam.jdi.soap.DneOnlineCalculator;
 import com.epam.jdi.soap.org.tempuri.*;
+import io.restassured.RestAssured;
+import io.restassured.config.HttpClientConfig;
+import io.restassured.config.RestAssuredConfig;
 import org.assertj.core.api.Assertions;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static com.epam.http.requests.ServiceInit.init;
 
 public class DneOnlineCalculatorTests {
+
+    @BeforeClass
+    public void initTimeouts() {
+        RestAssured.config = RestAssuredConfig.newConfig().httpClient(
+                HttpClientConfig.httpClientConfig()
+                        .setParam("http.connection.timeout",300000)
+                        .setParam("http.socket.timeout",300000)
+                        .setParam("http.connection-manager.timeout",300000)
+        );
+    }
 
     @BeforeMethod
     public void before() {

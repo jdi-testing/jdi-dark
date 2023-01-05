2820c2881ecb0e2f92441b4761a22d25399822f1
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2023-01-04 20:08:34 -0500

    Update Cucumber version

--- jdi-dark-bdd-tests/src/test/java/com/epam/jdi/httptests/CucumberTestRunner.java
+++ jdi-dark-bdd-tests/src/test/java/com/epam/jdi/httptests/CucumberTestRunner.java
@@ -6,7 +6,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"com/epam/jdi/httptests/steps", "com/epam/jdi/http/stepdefs/en"}
+        glue = {"com/epam/jdi/httptests/steps", "com/epam/jdi/http/stepdefs/en"},
+        plugin = {"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 )
 public class CucumberTestRunner {
 

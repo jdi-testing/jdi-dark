f11ed34c2dc05c24645444799e6f37ffdd78e009
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2023-01-05 00:33:32 -0500

    Update Cucumber version. Remove JUnit and TestNg

--- jdi-dark-bdd-tests/src/test/java/com/epam/jdi/httptests/steps/Hooks.java
+++ jdi-dark-bdd-tests/src/test/java/com/epam/jdi/httptests/steps/Hooks.java
@@ -1,10 +1,5 @@
 package com.epam.jdi.httptests.steps;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-
-import static com.epam.http.JdiHttpSettings.logger;
-
 public class Hooks {
 
 }

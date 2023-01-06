dd8e8374a334ad9fd7861b7c0795cd6286825806
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2023-01-06 15:34:45 -0500

    Quotes are added to the body, checking for contains

--- jdi-dark-tests/src/test/java/com/epam/jdi/httptests/examples/requestparams/DeleteTest.java
+++ jdi-dark-tests/src/test/java/com/epam/jdi/httptests/examples/requestparams/DeleteTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 import static com.epam.http.requests.RequestDataFactory.*;
 import static com.epam.http.requests.ServiceInit.init;
 import static com.epam.jdi.services.JettyService.*;
-import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.testng.Assert.assertEquals;
 
@@ -61,6 +61,6 @@ public class DeleteTest extends WithJetty {
     @Test
     public void deleteSupportsStringBody() {
         RestResponse response = deleteBody.call(body(TEST_BODY_VALUE));
-        response.assertThat().body(contains(TEST_BODY_VALUE));
+        response.assertThat().body(containsString(TEST_BODY_VALUE));
     }
 }
